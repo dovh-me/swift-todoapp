cc4ee2ccc5b204d1db5a3518100f6c14f56010c7
dovh-me <dovh.me@gmail.com>
2024-10-05 20:19:42 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Osura Httiarachchi on 2024-10-05.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
